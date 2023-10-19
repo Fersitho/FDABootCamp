d82eef3e1bd83129057fb53b9d83ea0c7deb7c72
Fernando Diaz Alaminos <diaz.alaminos.fernando@gmail.com>
2023-10-19 18:40:23 +0200

    clase 3 - parte 1

--- modl-js/src/index.js
+++ modl-js/src/index.js
@@ -1,28 +1,28 @@
-let animalEstado = 'pajaro feliz'
-let maxInsert = 8
-let nombres = []
+// let animalEstado = 'pajaro feliz'
+// let maxInsert = 8
+// let nombres = []
 
-for (let i = 0; i < maxInsert; i++) {
+// for (let i = 0; i < maxInsert; i++) {
 
-    let randomNumber = Math.random();
+//     let randomNumber = Math.random();
 
-    let minValue = 7.00;
-    let maxValue = 25.00;
+//     let minValue = 7.00;
+//     let maxValue = 25.00;
 
-    let scaledNumber = randomNumber * (maxValue - minValue) + minValue;
+//     let scaledNumber = randomNumber * (maxValue - minValue) + minValue;
 
-    let roundedNumber = scaledNumber.toFixed(2);
+//     let roundedNumber = scaledNumber.toFixed(2);
 
-    console.log(roundedNumber);
-    nombres.push({
-        num: i,
-        nombre: 'El Nombre-' + i,
-        size: roundedNumber + ' cm'
-    })
+//     console.log(roundedNumber);
+//     nombres.push({
+//         num: i,
+//         nombre: 'El Nombre-' + i,
+//         size: roundedNumber + ' cm'
+//     })
 
-}
+// }
 
-console.log('b contiene como valor:', nombres)
+// console.log('b contiene como valor:', nombres)
 
 document.getElementById('h1').innerText = 'Hola mundo'
 
@@ -76,31 +76,78 @@ console.log('Super count match string: ', superCount)
 
 let text2 = 'Hi, I am learning Javascript'
 
-let vowels2 = ['a','e','i','o','u','A','E','I','O','U']
+let vowels2 = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
- for (let j = 0; j < vowels2.length - 1; j++) {
+for (let j = 0; j < vowels2.length - 1; j++) {
 
     const vowelRegex = new RegExp(vowels2[j], 'gi');
-console.log(text2.match(vowelRegex))
+    console.log(text2.match(vowelRegex))
     if (text2.match(vowelRegex) != null && text2.match(vowelRegex).length > 0) {
-       
+
         text2 = text2.replaceAll(vowels2[j], '')
 
     }
-    
- }
 
- console.log('text2 =',text2)
- let text3 = 'Hi, I am learning Javascript'
- let vowels3 = 'aeiouAEIOU'
+}
 
- for (let x = 0; x < text3.length; x++) {
+console.log('text2 =', text2)
+let text3 = 'Hi, I am learning Javascript'
+let vowels3 = 'aeiouAEIOU'
+
+for (let x = 0; x < text3.length; x++) {
     let vowel = vowels3.charAt(x)
 
-    if(text3.includes(vowel)) {
-        text3 = text3.replaceAll(vowel,"")
+    if (text3.includes(vowel)) {
+        text3 = text3.replaceAll(vowel, "")
     }
 
- }
-console.log('Text3: ',text3)
+}
+console.log('Text3: ', text3)
 // }
+
+// clase 3
+
+//-- ejer1
+
+function formatText(text) {
+    return text.toLowerCase().replaceAll(' ', '');
+}
+
+function removeAccents(text) {
+    const accents = 'áéíóúÁÉÍÓÚ';
+    const vowels = 'aeiouAEIOU';
+
+    for (let i = 0; i < accents.length; i++) {
+        if (text.includes(accents.charAt(i))) {
+            text = text.replaceAll(accents.charAt(i), vowels.charAt(i));
+        }
+    }
+
+    return text;
+
+}
+
+function revertText(text) {
+    let textRevert = text;
+
+    for (let i = text.length - 1; i < 0; i++) {
+        textRevert += textRevert.charAt(i);
+    }
+
+    return textRevert;
+}
+
+function isPalindrome(text) {
+    
+    let textOriginFormated = removeAccents(formatText(text));
+    let textOriginFormatedRevert = revertText(textOriginFormated);
+
+    return textOriginFormated == textOriginFormatedRevert ? true : false;
+    
+}
+
+
+
+const texto = 'Dábale arroz a la zorra el abad'
+
+console.log('Es palindromo: '+texto+' ?', isPalindrome(texto))
