953b08029bdf6218002dd7a8d24b06975d745f8f
fer <diaz.alaminos.fernando@gmail.com>
2023-10-30 19:20:49 +0100

    mod 4 ejer1 eventHidden

--- modl-js/src/ejercicios/mod4/ejercicio1/index.js
+++ modl-js/src/ejercicios/mod4/ejercicio1/index.js
@@ -22,6 +22,54 @@ const moviesA = [{
         "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
 }]
 
+
+function eventHiddenDescription() {
+    
+    let elementos = document.querySelectorAll('.summary p')
+
+    for (let i = 0; i < elementos.length; i++) {
+        elementos[i].style.display = elementos[i].style.display == 'none' ? '-webkit-box' : 'none'
+    }
+
+}
+
+function eventHiddenData(event) {
+
+    let miElement = event.target
+    let stateView = miElement.nextElementSibling.style.display
+ // .parentNode nos da el padre!
+    if(stateView == 'none'){
+        miElement.nextElementSibling.style.display = 'flex'
+        miElement.className = 'fa-sharp fa-regular fa-eye-slash'
+    } else {
+        miElement.nextElementSibling.style.display = 'none'
+        miElement.className = 'fa-regular fa-eye'
+        
+    }
+    
+    // let elementos = document.querySelectorAll('section > div > article')
+    // let icons = document.querySelectorAll('section > i')
+    
+    // for (let i = 0; i < elementos.length; i++) {
+
+    //     elementos[i].toggleAttribute('hidden')
+
+    //     if (icons[i] !== undefined) {
+    //         if (elementos[i].hasAttribute('hidden')) {
+
+    //             icons[i].className = 'fa-sharp fa-regular fa-eye-slash'
+
+    //         } else {
+
+    //             icons[i].className = 'fa-regular fa-eye'
+
+    //         }
+    //     }
+
+    // }
+
+}
+
 const genSectionFilm = () => document.createElement('section')
 
 const genImgFilm = (film) => {
@@ -29,21 +77,29 @@ const genImgFilm = (film) => {
     let img = document.createElement('img')
     img.setAttribute('src', film.poster)
     img.setAttribute('alt', `Film: ${film.title}`)
+
     return img
 }
+const genIconView = () => {
+    let icon = document.createElement('i')
+    icon.className = 'fa-sharp fa-regular fa-eye-slash'
+    icon.addEventListener('click', eventHiddenData);
 
+    return icon
+}
 const genArticleRating = (film) => {
     const articleRating = document.createElement('article')
     articleRating.className = 'rating'
 
     let h3 = document.createElement('h3')
     h3.textContent = film.title
+
     articleRating.appendChild(h3)
 
     let p = document.createElement('p')
     p.textContent = `Rating: ${film.rating} / ${film.year}`
     articleRating.appendChild(p)
-    
+
     return articleRating;
 }
 
@@ -53,8 +109,14 @@ const genArticleSummary = (film) => {
 
     let h3 = document.createElement('h3')
     h3.textContent = 'Summary'
+
+
+    h3.addEventListener('click', eventHiddenDescription);
+
+
+
     articleSummary.appendChild(h3)
-    
+
     let p = document.createElement('p')
     p.textContent = film.description
     articleSummary.appendChild(p)
@@ -99,13 +161,19 @@ const createFilmsInfo = (movies) => {
         let film = movies[i]
         const sectionFilms = genSectionFilm()
         sectionFilms.appendChild(genImgFilm(film))
-        sectionFilms.appendChild(genArticleRating(film))
-        sectionFilms.appendChild(genArticleSummary(film))
+        sectionFilms.appendChild(genIconView())
 
-        sectionFilms.appendChild(genArticleDirector(film))
+        let divInfo = document.createElement('div');
+        divInfo.className = 'container-info-film'
+        divInfo.appendChild(genArticleRating(film))
+        divInfo.appendChild(genArticleSummary(film))
 
-        sectionFilms.appendChild(genArticleActors(film))
-        sectionFilms.appendChild(genArticleCategory(film))
+        divInfo.appendChild(genArticleDirector(film))
+
+        divInfo.appendChild(genArticleActors(film))
+        divInfo.appendChild(genArticleCategory(film))
+        sectionFilms.appendChild(divInfo)
+        
 
         document.querySelector('body>main').appendChild(sectionFilms)
 
@@ -113,4 +181,6 @@ const createFilmsInfo = (movies) => {
 
 }
 
-createFilmsInfo(movies.filter(film => film.actors.includes('Brad Pitt')))
+createFilmsInfo(
+    movies.filter(film => film.actors.includes('Brad Pitt')).sort((a,b) => b.year - a.year)
+)
