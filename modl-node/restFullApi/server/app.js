5bbe2dded22f90f00618f97270426d5181e0ec1b
fer <diaz.alaminos.fernando@gmail.com>
2023-12-11 17:42:23 +0100

    pacakege rertoca

--- modl-node/restFullApi/server/app.js
+++ modl-node/restFullApi/server/app.js
@@ -2,18 +2,19 @@ const http = require('http')
 
 const PORT = 3000
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer((req, res) => {
 
-    res.setHeader("Content-Type", "text/html")
+    res.setHeader("Content-Type", "text/html; charset=utf-8")
     res.end("<h1> Hola, esto es lo tipico </h1>")
 
 
 })
 
+
 server.listen(PORT, () => {
     console.log('Server is running')
-    
-        // open(`http://localhost:3000/`);
-      
+
+    // open(`http://localhost:3000/`);
+
 })
 
