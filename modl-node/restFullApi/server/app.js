82cf539b0ab96f172897807f4fb5dc3322c6a3dd
fer <diaz.alaminos.fernando@gmail.com>
2023-12-12 18:53:18 +0100

    clase 2 add connection mongodb

--- modl-node/restFullApi/server/app.js
+++ modl-node/restFullApi/server/app.js
@@ -4,17 +4,34 @@
 //     res.setHeader("Content-Type", "text/html; charset=utf-8")
 //     res.end("<h1> Hola, esto es lo tipico </h1>")
 // })
-
-const express = require('express')
+const mongoose = require("mongoose")
+const express = require("express")
 const PORT = 3000
-const userRouter = require('../routers/userRoutes')
+const userRouter = require("../routers/userRoutes")
 const app = express()
+
+require("dotenv").config();
+
+const urlMongo = process.env.DATABASE_URL_DEV
+
+mongoose.connect(urlMongo)
+const db = mongoose.connection
+
+db.on("error", (error) => {
+    console.log(`Error al conectar con mongo ${error}`)
+})
+db.on("connected", () => {
+    console.log(`Success connect to mongodb`)
+})
+db.on("disconnected", () => {
+    console.log(`Mongo disconnected`)
+})
 
 app.use(express.json())
 
 app.use("/users", userRouter)
 
 app.listen(PORT, () => {
-    console.log('Server is running')
+    console.log("Server is running")
 })
 
