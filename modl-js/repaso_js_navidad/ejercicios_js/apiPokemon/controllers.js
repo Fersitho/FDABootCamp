79f133642dd51e6a7f5a2649e7aa8a351f66c00e
fer <diaz.alaminos.fernando@gmail.com>
2024-01-08 16:38:59 +0100

    calculadora

--- modl-js/repaso_js_navidad/ejercicios_js/apiPokemon/controllers.js
+++ modl-js/repaso_js_navidad/ejercicios_js/apiPokemon/controllers.js
@@ -1,13 +1,12 @@
 import fetch from "node-fetch";
 export async function getPokemonByName() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/4");
-        // https://pokeapi.co/api/v2/pokemon/ditto
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        // https://pokeapi.co/api/v2/pokemon/4 
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const json = await response.json();
-        console.log(json);
     }
     catch (error) {
         console.error("Error fetching Pokemon:", error.message);
