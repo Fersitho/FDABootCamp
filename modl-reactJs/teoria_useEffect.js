d2cdbcfddac764751ff56232ef55bbb9866f9ea9
fer <diaz.alaminos.fernando@gmail.com>
2024-02-24 18:03:43 +0100

    fix component PlanificacionEstudio mode mensual

--- modl-reactJs/teoria_useEffect.js
+++ modl-reactJs/teoria_useEffect.js
@@ -1,12 +1,12 @@
 // El useEffect:
  
-// Tiene que ver con el cliclo de vida desde que se ejecuta hasta que se pinta y actualiza un estado, voy a dar ejemplos de react con componentes con clases. "las funciones que se usaban antes"
+// Tiene que ver con el cliclo de vida desde que se ejecuta hasta que se pinta y actualiza "" un estado, voy a dar ejemplos de react con componentes con clases. "las funciones que se usaban antes"
  
 // Se ejecuta antes de renderizar, antes teníamos la función "componentDidMount", ahora nos ejecuta el código de useEffect.
  
 // Tenemos una función para cuando desmontamos el componente!! --> antes era "componentWillUnmount" cuando se cierra el component ejecuta lo que tengamos dentro.
  
-// Y cuando se actualiza un estado, podemos especificar en un useEffect que se ejecute  el codigo, si tenemos este estando en el array de nuestro useEffect que controla como antes era "componentDidUpdate"
+// Y cuando se actualiza un estado, podemos especificar en un useEffect que se ejecute  el codigo, si tenemos este estando en el array de nuestro useEffect que controla como antes era "componentDidUpdate" tiene refrencia a useEffect
  
 // Esto me he aprendido entre ayer y hoy para explicar en una entrevista.
 
